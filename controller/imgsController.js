054893a9526da2e8db7a0da1c9fcf2ef8b9b0c0a
MustaphaBousilDev <112890579+MustaphaBousilDev@users.noreply.github.com>
2023-07-29 21:11:53 +0100

    finish add and update images from database and delete imgs from cloudinaryr

---
+++ controller/imgsController.js
@@ -0,0 +1,128 @@
+
+const cloudinary=require('../helpers/cloudinary')
+
+const Images=require('../model/imagesModel')
+const fs = require('fs');
+const asyncHandler= require('express-async-handler')
+const validateMongoDbId = require('../helpers/validateMongoDB')
+
+const {
+     validateStatus,
+     validateString,
+     validateSizeEnum,
+     validateNumberIntegers,
+     validateNumber,
+     validateColorEnum,
+     validateImage,
+}=require('../helpers/validations')
+
+
+//-----------------------------------------------------
+const  createImgsProduct=asyncHandler(async(req,res)=>{
+     try{
+          let pictureFiles = req.files;
+          if (!pictureFiles){
+             return res.status(400).json({ message: "No picture attached!" });
+          }
+          
+          let multiplePicturePromise = pictureFiles.map((picture) =>
+                  cloudinary.v2.uploader.upload(picture.path)
+          );
+          let imgs=[]
+          let counter=0
+          
+             let imageResponses = await Promise.all(multiplePicturePromise);
+             imageResponses.map((imageResponse)=>{
+                  let keyImgs={
+                       picture:imageResponse.secure_url,
+                       cloudinary_id:imageResponse.public_id,
+                       user:req.user._id
+                  }
+                  imgs[counter]=keyImgs
+                  counter++
+             })
+             //save images in database
+             const images=await Images.insertMany(imgs)
+             //return id of this images 
+             const ids=images.map((image)=>image._id)
+          //remove file from server
+          pictureFiles.map((picture) => fs.unlinkSync(picture.path))
+          res.status(200).json({ images: ids });
+       }catch(err){
+           res.status(500).json({
+             message: err.message,
+           });
+       }
+}) 
+
+const getAllImgsProduct=asyncHandler(async(req,res)=>{
+     try{
+          const allImgs=await Images.find({},{
+               _id:1,
+               picture:1,
+               cloudinary_id:1,
+               status:1,
+               user:1,
+               user_updated:
+
+               {
+                    $cond: { if: { $eq: [ "$user_updated", null ] }, then: 0, else: 1 }
+               },
+               createdAt:1,
+          }).populate('user',{
+               _id:1,
+               email:1
+          })
+          res.json(allImgs)
+     }catch(error){
+          throw new Error(error)
+     }
+})
+const updateImgsProduct = asyncHandler(async (req, res) => {
+     const {id}=req.params 
+     let picture =req.file;
+     console.log('picture')
+     console.log(picture)
+     validateMongoDbId(id);
+     //console.log('fuck you')
+     //get image has this id and get the cloudinary_id
+     
+     /* {
+          
+          const ImagesDB= await Images.findByIdAndUpdate(id,{
+               name:req?.body?.name,
+               category:req?.body?.category,
+               user_updated:req?.body?.user_updated,
+               slug:req?.body?.slug,
+               status:req?.body?.status,
+     },{new: true});*/
+     try{
+          const img=await Images.findById(id)
+          const cloudinary_ids=img.cloudinary_id
+          //delete from cloudinary 
+          console.log(cloudinary_ids)
+          cloudinary.uploader.destroy(cloudinary_ids)
+          console.log('rtoortort')
+          const result = await cloudinary.uploader.upload(picture.path);
+          console.log('new img')
+          console.log(result)
+          const imgsUpdated=await Images.findByIdAndUpdate(id,{
+               picture:result.secure_url,
+               cloudinary_id:result.public_id,
+               user_updated:req.user._id,
+               
+          },{new: true})
+          console.log('imgsUpdated')
+          console.log(imgsUpdated)
+          fs.unlinkSync(picture.path);
+          res.json(imgsUpdated)
+     } catch (error) {
+       throw new Error(error);
+     }
+});
+
+module.exports={
+     createImgsProduct,
+     getAllImgsProduct,
+     updateImgsProduct
+}
--- router/imgsRoute.js
+++ router/imgsRoute.js
@@ -1,56 +1,19 @@
 const express=require('express')
 const {authMiddleware,isAdmin, isGood}=require('../middleware/authMiddleware')
-
 const upload=require('../middleware/multer')
-const cloudinary=require('../helpers/cloudinary')
 //fs is file system using for delete file from server
-const fs=require('fs')
+
 const router=express.Router()
+const {
+     createImgsProduct,
+     getAllImgsProduct,
+     updateImgsProduct
+}=require('../controller/imgsController')
 
-/*router.post('/',upload.array('picture'), async (req,res)=>{
 
-     try{
-               let urls = [];
-               let c=0
-               const uploader = req.files.map(async (file) => {
-                    
-                    await cloudinary.uploads(file.path);
-                    urls.push({url:file.path})
-                    fs.unlinkSync(file.path)
-                    c++
-                    
-               });
-               uploader()
-               
-     }catch(error){
-          console.error('Error uploading images:', error);
-          res.status(500).json({
-               message: 'An error occurred while uploading images',
-               error: error.message
-          });
-     }
-})*/
-
-router.post("/",isAdmin, upload.array("picture"), async (req, res) => {
-
-    try{
-       let pictureFiles = req.files;
-       if (!pictureFiles){
-          return res.status(400).json({ message: "No picture attached!" });
-       }
-       let multiplePicturePromise = pictureFiles.map((picture) =>
-               cloudinary.v2.uploader.upload(picture.path)
-       );
-       let imageResponses = await Promise.all(multiplePicturePromise);
-       //remove file from server
-       pictureFiles.map((picture) => fs.unlinkSync(picture.path))
-       res.status(200).json({ images: imageResponses });
-    }catch(err){
-        res.status(500).json({
-          message: err.message,
-        });
-    }
-  });
+router.post("/",isAdmin, upload.array("picture"),createImgsProduct);
+router.get('/',isAdmin,getAllImgsProduct)
+router.put('/:id',isAdmin,upload.single('picture'),updateImgsProduct)
 
 //filtering and sorting category
 
